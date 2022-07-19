f65c5ab2e0b94e6ea6c4ff95ffa36d7e3f1dcd99
Juan Carlos <jchernandez827@gmail.com>
2022-07-19 18:49:07 -0500

    Add styles for intro component

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,5 +1,11 @@
 import "../styles/intro.css";
 
-const Intro = () => <div>Intro</div>;
+const Intro = () => ((
+  <div className="intro-container">
+    <h1>Quizzical</h1>
+    <p>Test your knowledge !</p>
+    <button className="intro-btn">Start quiz</button>
+  </div>
+));
 
 export default Intro;
