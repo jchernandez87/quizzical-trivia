abee32abf8a7aa301fac4d7bff131c42ec0c5bc7
Juan Carlos <jchernandez827@gmail.com>
2022-07-19 23:02:43 -0500

    Add fucntion to randomize answers

--- src/components/questionsCard.js
+++ src/components/questionsCard.js
@@ -2,7 +2,20 @@ import "../styles/questionsCard.css";
 import { nanoid } from 'nanoid'
 
 const QuestionCard = (props) => {
-  const testing = props.answers.map(item => <span key={nanoid()}>{item}</span>)
+  
+  const shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array
+  }
+
+  const randomArr = shuffleArray(props.answers);
+
+  const testing = randomArr.map(item => <span key={nanoid()}>{item}</span>)
 
   return (
     <div className="card-container">
